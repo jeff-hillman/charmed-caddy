c514d5bebfe2ef907754254ed74d56590168959b
jeff-hillman <jred55@gmail.com>
2021-09-16 20:55:21 +0000

    mods based off of template, appears to be stable.  still need Caddyfile

--- src/charm.py
+++ src/charm.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2021 ubuntu
+# Copyright 2021 Jon Seager
 # See LICENSE file for licensing details.
 #
 # Learn more at: https://juju.is/docs/sdk
@@ -13,11 +13,12 @@ develop a new k8s charm using the Operator Framework:
 """
 
 import logging
+import urllib
 
+from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
 from ops.charm import CharmBase
-from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, MaintenanceStatus, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
@@ -25,22 +26,59 @@ logger = logging.getLogger(__name__)
 class CaddyCharm(CharmBase):
     """Charm the service."""
 
-    _stored = StoredState()
-
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.caddy_pebble_ready, self._on_caddy_pebble_ready)
+        self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(hostname=[])
+        self.framework.observe(self.on.pull_site_action, self._pull_site_action)
 
-    def _on_caddy_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
+        self.ingress = IngressRequires(
+            self,
+            {
+                "service-hostname": self._hostname,
+                "service-name": self.app.name,
+                "service-port": 8080,
+            },
+        )
+
+    @property
+    def _hostname(self):
+        """Return the external hostname to be passed to ingress via the relation."""
+        # It is recommended to default to `self.app.name` so that the external
+        # hostname will correspond to the deployed application name in the
+        # model, but allow it to be set to something specific via config.
+        return self.config["hostname"] or self.app.name
+
+    def _on_install(self, _):
+        # Download the site
+        self._fetch_site()
+
+    def _on_config_changed(self, event):
+        """Handle the config-changed event"""
+        # Get the caddy container so we can configure/manipulate it
+        container = self.unit.get_container("caddy")
+        # Create a new config layer
+        layer = self._caddy_layer()
+
+        if container.can_connect():
+            # Get the current config
+            services = container.get_plan().to_dict().get("services", {})
+            # Check if there are any changes to services
+            if services != layer["services"]:
+                # Changes were made, add the new layer
+                container.add_layer("caddy", layer, combine=True)
+                logging.info("Added updated layer 'caddy' to Pebble plan")
+                # Restart it and report a new status to Juju
+                container.restart("caddy")
+                logging.info("Restarted caddy service")
+            # All is well, set an ActiveStatus
+            self.unit.status = ActiveStatus()
+        else:
+            self.unit.status = WaitingStatus("waiting for Pebble in workload container")
+
+    def _caddy_layer(self):
+        """Returns a Pebble configration layer for Caddy"""
+        return {
             "summary": "caddy layer",
             "description": "pebble config layer for caddy",
             "services": {
@@ -49,71 +87,30 @@ class CaddyCharm(CharmBase):
                     "summary": "caddy",
                     "command": "caddy start",
                     "startup": "enabled",
-                    "environment": {"hostname": self.model.config["hostname"]},
+                    "environment": {
+                        "HOSTNAME": self.config["hostname"],
+                        "WEBROOT": "/srv",
+                    },
                 }
             },
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("caddy", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
+
+    def _fetch_site(self):
+        """Fetch latest copy of website from Github and move into webroot"""
+        # Set the site URL
+        site_src = "https://jnsgr.uk/demo-site"
+        # Set some status and do some logging
+        self.unit.status = MaintenanceStatus("Fetching web site")
+        logger.info("Downloading site from %s", site_src)
+        # Download the site
+        urllib.request.urlretrieve(site_src, "/srv/index.html")
+        # Set the unit status back to Active
         self.unit.status = ActiveStatus()
 
-    def _caddy_layer(self):
-        """Returns a Pebble configration layer for Caddy"""
-        return {
-           "summary": "caddy layer",
-            "description": "pebble config layer for caddy",
-            "services": {
-               "caddy": {
-                    "override": "replace",
-                    "summary": "caddy",
-                    "command": "caddy start",
-                    "startup": "enabled",
-                    "environment": {"hostname": self.model.config["hostname"]},
-               }
-           },
-        }
-    def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
-        """
-        current = self.config["hostname"]
-        if current not in self._stored.hostname:
-            logger.debug("found a new hostname: %r", current)
-            self._stored.hostname.append(current)
-        container = self.unit.get_container("caddy")
-        layer = self._caddy_layer()
-        services = container.get_plan().to_dict().get("services", {})
-        if services != layer["services"]:
-            # Changes were made, add the new layer
-            container.add_layer("caddy", layer, combine=True)
-            logging.info("Added updated layer 'caddy' to Pebble plan")
-            # Stop the service if it is already running
-            if container.get_service("caddy").is_running():
-                container.stop("caddy")
-            # Restart it and report a new status to Juju
-            container.start("caddy")
-            logging.info("Restarted caddy service")
-        # All is well, set an ActiveStatus
-        self.unit.status = ActiveStatus()
-
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
-        else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+    def _pull_site_action(self, event):
+        """Action handler that pulls the latest site archive and unpacks it"""
+        self._fetch_site()
+        event.set_results({"result": "site pulled"})
 
 
 if __name__ == "__main__":
